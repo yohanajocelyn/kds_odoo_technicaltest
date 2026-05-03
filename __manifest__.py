084f4fc4340539d80102270aae16a224ea63a770
yohanajocelyn <yohanajoycelyn@gmail.com>
2026-05-03 19:00:45 +0700

    update manifest file

--- __manifest__.py
+++ __manifest__.py
@@ -4,15 +4,14 @@
     'summary': 'Adds a priority field to the contact form (Technical Test KDS)',
     'sequence': 10,
     'description': """
-        This module allows users to set a priority level (Low, Medium, High) 
+        This module allows users to set a priority level (Low, Medium, High, Urgent) 
         for each contact, appearing as stars on the contact form.
     """,
     'category': 'Tools',
     'author': 'Yohana',
-    'website': 'https://github.com/yohanajocelyn',
     'depends': ['base', 'contacts'],  # CRITICAL: Ensures 'contacts' is installed first
     'data': [
-        'views/res_partner_view.xml',  # Loads your XML UI changes
+        'views/views.xml',  # Loads your XML UI changes
     ],
     'installable': True,
     'application': True,
